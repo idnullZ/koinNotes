94a622474126ddcdb83cda6761ece49548a43ee5
zunun <mr.zunun1@gmail.com>
2021-10-27 22:25:53 +0600

    Add All ViewModel

--- app/src/main/java/idnull/z/koinnotes/data/AppModules.kt
+++ app/src/main/java/idnull/z/koinnotes/data/AppModules.kt
@@ -3,13 +3,17 @@ package idnull.z.koinnotes.data
 
 import idnull.z.koinnotes.data.room.AppRoomDatabase
 import idnull.z.koinnotes.data.room.AppRoomRepository
+import idnull.z.koinnotes.presentation.add_new_note.AddNewNoteViewModel
 import idnull.z.koinnotes.presentation.main.MainFragmentViewModel
+import idnull.z.koinnotes.presentation.note.NoteFragmentViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MainFragmentViewModel(get()) }
+    viewModel { AddNewNoteViewModel(get()) }
+    viewModel { NoteFragmentViewModel(get()) }
 }
 
 val repositoryModule = module {
